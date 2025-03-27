4701e6a8503fbb15093e49f1d97de9e0d0feb93b
Sayan Snigdha Pal <sayanpalb@gmail.com>
2025-03-27 20:13:25 +0530

    update in index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./db/db");
+const authRoutes = require("./routes/user");
+
+dotenv.config();
 
 const app = express();
 app.use(cors);
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/auth", authRoutes);
 
 const PORT = 5000;
 app.listen(() => {
